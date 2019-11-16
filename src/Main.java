c259a349afbd0d4c95a75a1aff328e287ee7bf20
migapipo <670475225@qq.com>
2019-11-16 23:13:44 +1100

    updated Robot class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -20,9 +19,45 @@ public class Main {
 
             System.out.println("User input is " + userInput);
 
+            ArrayList<String> givenCommandList = parseGivenCommand(userInput);
 
+            for (int i =0; i < givenCommandList.size();i++){
+                System.out.println(givenCommandList.get(i));
+                for (int j=0; j < givenCommandList.get(i).length();j++){
+                    System.out.println(givenCommandList.get(i).charAt(j));
+                }
+
+            }
+
+//          System.out.println(givenCommandList.get(0).charAt(0));
+//          System.out.println(givenCommandList.get(0).charAt(1));
+//          System.out.println(givenCommandList.get(1).charAt(0));
+//          System.out.println(givenCommandList.get(1).charAt(1));
+
+    }
+
+
+    /**
+     * Parse the given route from the string into the ArrayList in order to implement the calculation functions on the
+     * given route
+     *
+     * @param givenCommandString the string of given route
+     * @return the ArrayList of the given route
+     */
+    public static  ArrayList<String> parseGivenCommand(String givenCommandString) {
+        ArrayList<String> commandList = new ArrayList<>();
+        for (String string : givenCommandString.split(",")) {
+            String singleCommandName = string;
+            commandList.add(singleCommandName);
+        }
+        return commandList;
+    }
+
+
+    public static void moveRoboteByCommand(Robot robot, ArrayList<String> command){
 
 
 
     }
+
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,41 @@
+
 public class Robot {
 
+    int coordinateX;
+    int coordinateY;
+
+    int facingDirection;
+
+    public Robot(int coordinateX, int coordinateY, int facingDirection) {
+
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.facingDirection = facingDirection;
+
+    }
+
+    public int getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(int coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+    public int getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(int coordinateY) {
+        this.coordinateY = coordinateY;
+    }
+
+    public int getFacingDirection() {
+        return facingDirection;
+    }
+
+    public void setFacingDirection(int facingDirection) {
+        this.facingDirection = facingDirection;
+    }
 
 }
